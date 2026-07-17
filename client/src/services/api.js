a1accc49b111c5e42d073672c1ef2fb6aa12ab4d
sariga <sec24it122@sairamtap.edu.in>
2026-07-17 20:24:42 +0530

    Updated API URL for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://research-paper-repository-final.onrender.com",
 });
 
 export default api;
