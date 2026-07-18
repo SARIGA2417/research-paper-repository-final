f60754073b02d10139ff41973bd3eeed2cc60d3a
sariga <sec24it122@sairamtap.edu.in>
2026-07-18 20:00:49 +0530

    Added vercel.json

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://research-paper-repository-final.onrender.com",
+baseURL:
+"https://research-paper-repository-final.onrender.com/api",
 });
 
 export default api;
